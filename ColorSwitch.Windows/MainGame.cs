38953cbbf41e277b8109c719c55986573e09fbdd
crt09 <dr.light2215@gmail.com>
2019-02-05 00:55:31 +0300

    Initial commit

---
+++ ColorSwitch.Windows/MainGame.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ColorSwitch.Windows {
+	public class MainGame : Game {
+		GraphicsDeviceManager graphics;
+
+		public MainGame() {
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+
+			graphics.PreferredBackBufferWidth = 500;
+			graphics.PreferredBackBufferHeight = 800;
+			graphics.PreferMultiSampling = true;
+			graphics.ApplyChanges();
+
+			IsMouseVisible = true;
+		}
+
+		protected override void Draw(GameTime gameTime) {
+			GraphicsDevice.Clear(Color.Black);
+			base.Draw(gameTime);
+		}
+	}
+}
--- ColorSwitch.Windows/Program.cs
+++ ColorSwitch.Windows/Program.cs
@@ -14,7 +14,7 @@ namespace ColorSwitch.Windows
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new MainGame())
                 game.Run();
         }
     }
