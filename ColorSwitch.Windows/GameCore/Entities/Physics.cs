46b1d8e568fb3038ae8f3701d1079579cc2a3e6d
crt09 <dr.light2215@gmail.com>
2019-02-28 15:19:02 +0300

    Add physical player

---
+++ ColorSwitch.Windows/GameCore/Entities/Physics.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+
+namespace ColorSwitch.Windows.GameCore.Entities {
+	public class Physics {
+
+		public Physics(Entity entity) {
+			this.entity = entity;
+		}
+
+		public void ApplyImpulse() {
+			impulseAccelerator = 0;
+			impulseHandling = true;
+		}
+        
+        private Entity entity;
+        private bool impulseHandling;
+		private float impulseAccelerator;
+
+		private static float ImpulseSpeed => 10f;
+
+		public void Update() {
+			if (impulseHandling) {
+				if (impulseAccelerator < 10f) {
+					entity.transform.position -= new Vector2(0, ImpulseSpeed - impulseAccelerator);
+					impulseAccelerator += 1.0f;
+				} else {
+					impulseHandling = false;
+				}
+			}
+			else {
+				entity.transform.position += new Vector2(0, ImpulseSpeed - impulseAccelerator);
+                if (impulseAccelerator > 0)
+					impulseAccelerator -= 0.5f;
+			}
+		}
+	}
+}
--- ColorSwitch.Windows/GameCore/Entities/Player.cs
+++ ColorSwitch.Windows/GameCore/Entities/Player.cs
@@ -8,20 +8,37 @@ namespace ColorSwitch.Windows.GameCore.Entities {
 	public class Player : Entity {
 		public Player() : base("player") { }
 
+		public Color color {
+			get => playerSprite.color;
+			set => playerSprite.color = value;
+        }
+
+		private Sprite playerSprite;
 		private Texture2D playerTexture;
+		private Physics physicsHandler;
+
+		private bool gameStarted = false;
 
 		public override void onAddedToScene() {
+			physicsHandler = new Physics(this);			
+
 			playerTexture = scene.content.Load<Texture2D>("Player/player_circle");
-			addComponent(new Sprite(playerTexture));
-			transform.position = new Vector2(scene.sceneRenderTargetSize.X / 2, scene.sceneRenderTargetSize.Y / 2);
+			playerSprite = new Sprite(playerTexture);
+
+            addComponent(playerSprite);
+			transform.position = new Vector2(scene.sceneRenderTargetSize.X / 2, scene.sceneRenderTargetSize.Y / 2);	
 		}
 
 		public override void update() {
 			base.update();
-			if (Input.isKeyPressed(Keys.Up)
-			    || Input.leftMouseButtonPressed) {
-				// jump
+
+			if(gameStarted)
+				physicsHandler.Update();
+
+			if (Input.isKeyPressed(Keys.Up) || Input.leftMouseButtonPressed) {
+				gameStarted = true;
+				physicsHandler.ApplyImpulse();
 			}
 		}
-	}
+    }
 }
